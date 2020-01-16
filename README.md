619d2a4d1d8173ed7ce4dfb227be8efc0fd2d71d
Fitti <dev@fitti.io>
2020-01-16 22:00:56 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Words
+Was helping a friend create a list of 4 letter words from a list of 6 letters.
---
+++ words.py
@@ -0,0 +1,26 @@
+#!/bin/python
+from sys import argv
+# Permutations.
+# W, L, P, B, M, E
+
+words = []
+
+# Item must be comprised of 4 ingredients.  Repeat ingredients are acceptable.
+letters = ['W', 'L', 'P', 'B', 'M', 'E']
+
+def make_all_words(list, word_length, word=""):
+    if len(word) == int(word_length):
+        words.append(word)
+    else:
+        index = len(word)
+        for letter in list:
+            if index == len(word):
+                word += letter
+            else:
+                word = word[:len(word) - 1] + letter
+            make_all_words(list, word_length, word)
+
+make_all_words(letters, int(argv[1]))
+
+for item in words:
+    print(item)
