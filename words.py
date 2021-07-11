24b431f593ea11d9a9bcb6672e58f4c8c1e7ca6a
Fitti <dev@fitti.io>
2021-07-11 19:10:47 +0200

    Fix style

--- words.py
+++ words.py
@@ -8,6 +8,7 @@ words = []
 # Item must be comprised of 4 ingredients.  Repeat ingredients are acceptable.
 letters = ['W', 'L', 'P', 'B', 'M', 'E']
 
+
 def make_all_words(list, word_length, word=""):
     if len(word) == word_length:
         words.append(word)
@@ -19,6 +20,7 @@ def make_all_words(list, word_length, word=""):
             else:
                 word = word[:len(word) - 1] + letter
             make_all_words(list, word_length, word)
+
 
 make_all_words(letters, int(argv[1]))
 
